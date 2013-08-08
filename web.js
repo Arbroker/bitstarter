a48bbe68fc8484a6c4a7bb3bcbb7a6712fc7a93c
Armend Lulani <armendve@gmail.com>
2013-08-08 01:30:24 +0000

    Edited product dex in index.html

--- web.js
+++ web.js
@@ -21,7 +21,7 @@ app.get("/", indexFile);
 app.get("/index.html", indexFile);
 app.get("/test.html", readHtml);
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
     console.log("Listening on " + port);
 });
